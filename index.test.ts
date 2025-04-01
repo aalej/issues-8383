57e9dd51084ffa8262d46a57d35dbdad8cd290f5
aalej <alejandromarco@google.com>
2025-04-02 02:30:08 +0800

    first commit

---
+++ index.test.ts
@@ -0,0 +1,29 @@
+const {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+} = require("@firebase/rules-unit-testing");
+const fs = require("node:fs");
+
+for (let i = 0; i < 1000; i++) {
+  describe(`Test number ${i}`, () => {
+    beforeAll(async () => {
+      await initializeTestEnvironment({
+        projectId: "demo-project",
+        firestore: {
+          rules: fs.readFileSync("firestore.rules", "utf8"),
+          host: "localhost",
+          port: 8080,
+        },
+        hub: {
+          host: "localhost",
+          port: 4400,
+        },
+      });
+    });
+
+    it("Should add 1+1", () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+}
