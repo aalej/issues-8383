fb6f2ea58d3573a610eb5154f2d799329b18d606
aalej <alejandromarco@google.com>
2025-04-04 00:27:49 +0800

    Scale tests up to 50

--- __tests__/index_047.spec.js
+++ __tests__/index_047.spec.js
@@ -1,20 +1,13 @@
-// I don't like re-wrtting over and over again
-
-const fs = require("node:fs");
-const { join } = require("node:path");
-
-const outPath = "__tests__"
-
-const TEST_TEMPLATE = `const {
+const {
     assertSucceeds,
     initializeTestEnvironment,
 } = require("@firebase/rules-unit-testing");
 const { setDoc, doc, getDoc } = require("firebase/firestore");
 const fs = require("node:fs");
 
-for (let i = 0; i < {{MAX_COUNT}}; i++) {
+for (let i = 0; i < 100; i++) {
     const testCount = i.toString().padStart(3, "0")
-    describe(\`Test {{TEST_NUMBER}} - \${testCount}\`, () => {
+    describe(`Test 047 - ${testCount}`, () => {
         let testEnv = null;
 
         beforeAll(async () => {
@@ -53,19 +46,4 @@ for (let i = 0; i < {{MAX_COUNT}}; i++) {
         });
     });
 
-}
-`
-
-const TEST_SCRIPT_MAX = 50; // Number of test script to be created
-const TEST_RUNS_MAX = 100; // Times the test will be run
-
-
-for (let i = 1; i <= TEST_SCRIPT_MAX; i++) {
-    const testNumber = i.toString().padStart(3, "0")
-    let testContent = TEST_TEMPLATE;
-    testContent = testContent
-        .replace("{{MAX_COUNT}}", TEST_RUNS_MAX)
-        .replace("{{TEST_NUMBER}}", testNumber);
-
-    fs.writeFileSync(join(outPath, `index_${testNumber}.spec.js`), testContent)
 }
